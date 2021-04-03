e646b729c36a5fa5ad77f608a370fb497e988fb2
Sam <hatem@clearrain.xyz>
2021-04-03 17:17:47 -0400

    update symbol to BOX

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -18,7 +18,7 @@ export const CHAIN_ID = isProd ? ChainId.MAINNET : ChainId.GOERLI
 
 export const INITIAL_SUPPLY = 2100
 
-export const TOKEN_SYMBOL = 'BKFT'
+export const TOKEN_SYMBOL = 'BOX'
 
 export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
 
