cfa1753d24de0698eac33aaa6c0fcf90c13fbdc1
Sam <sam.hatem17@gmail.com>
2021-04-15 23:55:45 -0400

    fix: frameborder => frameBorder

--- src/components/Gallery/3DGallery.js
+++ src/components/Gallery/3DGallery.js
@@ -5,7 +5,7 @@ import { GalleryFrame } from '.'
 export default function Gallery3D() {
   return (
     <GalleryFrame>
-      <iframe title="Box" id="d71735aa-36cc-4b13-93ab-bf801385d613" src="https://www.vectary.com/viewer/v1/?model=d71735aa-36cc-4b13-93ab-bf801385d613&env=cedarbridge&turntable=3" frameborder="0" width="100%" height="480" />
+      <iframe title="Box" id="d71735aa-36cc-4b13-93ab-bf801385d613" src="https://www.vectary.com/viewer/v1/?model=d71735aa-36cc-4b13-93ab-bf801385d613&env=cedarbridge&turntable=3" frameBorder="0" width="100%" height="480" />
     </GalleryFrame>
   )
 }
