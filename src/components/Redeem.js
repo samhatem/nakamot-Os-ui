281f715008a6b62fcf48e307b2edca89fe75dd32
Sam <sam.hatem17@gmail.com>
2021-04-15 23:58:31 -0400

    fix: remove log for debugging

--- src/components/Redeem.js
+++ src/components/Redeem.js
@@ -76,8 +76,6 @@ export default function Redeem({
 
   const { supply: nftSupply } = useNftTotalSupply()
 
-  console.log({ nftSupply });
-
   const pending = !!transactionHash
 
   useEffect(() => {
